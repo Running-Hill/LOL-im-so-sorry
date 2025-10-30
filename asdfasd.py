4f08e3b93442ca17b1f61765e8b6295582768423
Lusitania <azzuroprovenza@gmail.com>
2025-10-30 23:20:03 +0530

    Add files via upload

---
+++ asdfasd.py
@@ -0,0 +1,18 @@
+import string
+import time
+import base64
+
+encoded = b'aGVsbG8gd29ybGQgaW0gZ2F5aGFyaq=='
+text = base64.b64decode(encoded).decode()
+
+temp = ''
+for ch in text:
+    for i in string.printable:
+        if i == ch or ch == ' ':
+            time.sleep(0.02)
+            print(temp + i)
+            temp += ch
+            break
+        else:
+            time.sleep(0.02)
+            print(temp + i)
